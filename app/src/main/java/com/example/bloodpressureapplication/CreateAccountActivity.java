13e8105afd787d3cbb33ab9b0fbe04c898ce201c
Erin Rutkowski-jones <100587683@oncampus.local>
2019-11-14 19:54:50 -0500

    updated createAccount and login activities for gathering inputs from the user

--- app/src/main/java/com/example/bloodpressureapplication/CreateAccountActivity.java
+++ app/src/main/java/com/example/bloodpressureapplication/CreateAccountActivity.java
@@ -5,14 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CreateAccountActivity extends AppCompatActivity {
+
+    EditText name_txt;
+    EditText email_txt;
+    EditText password_txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
+        name_txt = (EditText)findViewById(R.id.name_txt);
+        email_txt = (EditText)findViewById(R.id.email_txt);
+        password_txt = (EditText)findViewById(R.id.password_txt);
     }
 
     public void back(View view) {
@@ -21,8 +30,23 @@ public class CreateAccountActivity extends AppCompatActivity {
     }
 
 
-    public void mainPage(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+    public void signup(View view) {
+        //get the information entered
+        String name = name_txt.getText().toString();
+        String email = email_txt.getText().toString();
+        String password = password_txt.getText().toString();
+
+        //make sure each section is filled out
+        if(name.equals("") || email.equals("") || password.equals("")){
+            Toast.makeText(this, "Please enter all three fields", Toast.LENGTH_LONG).show();
+        }
+
+        else{
+            //send new information to the database
+
+            //move to the main activity
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/bloodpressureapplication/LoginActivity.java
+++ app/src/main/java/com/example/bloodpressureapplication/LoginActivity.java
@@ -5,13 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public EditText email;
+    public EditText password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = (EditText)findViewById(R.id.email_txt);
+        password = (EditText)findViewById(R.id.password_txt);
     }
 
     public void createAccount(View view) {
@@ -22,18 +31,24 @@ public class LoginActivity extends AppCompatActivity {
 
     public void login(View view) {
         //check that both fields have been filled out
+        String em = email.getText().toString();
+        String pass = password.getText().toString();
 
         //if not both filled out, notify the user
-        //Toast.makeText(this, "Please enter both a username and a password", Toast.LENGTH_LONG).show();
+        if ((em.equals("")) || (pass.equals(""))){
+            Toast.makeText(this, "Please fill in email and password", Toast.LENGTH_LONG).show();
+        }
+        else{
+            //check if login information is correct against the database
 
-        //check if login information is correct against the database
+            //if incorrect, send toast message that it's wrong
+            //Toast.makeText(this, "Username or password incorrect", Toast.LENGTH_LONG).show();
 
-        //if incorrect, send toast message that it's wrong
-        //Toast.makeText(this, "Username or password incorrect", Toast.LENGTH_LONG).show();
+            //if correct start new activity with username bundled
+            Intent intent = new Intent(this, MainActivity.class);
+            //intent.putExtra("username", username);
+            startActivity(intent);
+        }
 
-        //if correct start new activity with username bundled
-        Intent intent = new Intent(this, MainActivity.class);
-        //intent.putExtra("username", username);
-        startActivity(intent);
     }
 }
