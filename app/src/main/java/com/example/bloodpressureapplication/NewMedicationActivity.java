e813c3bcd08fe7c71c5b2e33a9b5f59405d6cc99
AlexMotyka <motykaalex@gmail.com>
2019-11-26 15:55:56 -0500

    Add save, cancel, and onCheckboxClicked methods

--- app/src/main/java/com/example/bloodpressureapplication/NewMedicationActivity.java
+++ app/src/main/java/com/example/bloodpressureapplication/NewMedicationActivity.java
@@ -3,12 +3,28 @@ package com.example.bloodpressureapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class NewMedicationActivity extends AppCompatActivity {
+
+    public EditText medName;
+    public EditText timeVal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_medication);
+    }
+
+    public void cancel(View view) {
+
+    }
+
+    public void save(View view) {
+    }
+
+    public void onCheckboxClicked(View view) {
+        
     }
 }
