5a576daf265cf2b39265aa218a0fbe68de7e63a7
Kalev Gonvick <100582575@oncampus.local>
2019-11-28 15:28:05 -0500

    fixed graph

--- app/src/main/java/com/example/bloodpressureapplication/Fragments/GraphFragment.java
+++ app/src/main/java/com/example/bloodpressureapplication/Fragments/GraphFragment.java
@@ -38,12 +38,14 @@ import com.example.bloodpressureapplication.R;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GraphFragment extends Fragment {
     private AnyChartView anyChartView;
     private Cartesian cartesian;
     private List<DataEntry> seriesData;
+    private List<DataEntry> seriesData2;
     private RequestQueue queue;
     @Nullable
     @Override
@@ -51,25 +53,82 @@ public class GraphFragment extends Fragment {
         View graph_view = inflater.inflate(R.layout.fragment_graph, null);
         anyChartView = graph_view.findViewById(R.id.any_chart_view);
         anyChartView.setProgressBar(graph_view.findViewById(R.id.progress_bar));
-
+        cartesian = AnyChart.line();
         queue = Volley.newRequestQueue(getContext());
 
-        Cartesian cartesian = AnyChart.line();
-        cartesian.animation(true);
-        cartesian.padding(10d, 20d, 5d, 20d);
-        cartesian.crosshair().enabled(true);
-        cartesian.crosshair().yLabel(true);
-        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
-        cartesian.title("Blood Pressure Trend");
-        cartesian.yAxis(0).title("Date(Days)");
-        cartesian.xAxis(0).labels().padding(5d, 5d, 5d, 5d);
 
-        final String url = server.url+"getBP"+"?email="+Person.email;
+
+        final String url = server.url+"getBP"+"?clientID="+Person.clientID;
         StringRequest getRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        parseAllData(response, seriesData);
+                        cartesian.animation(true);
+
+                        cartesian.padding(10d, 20d, 5d, 20d);
+
+                        cartesian.crosshair().enabled(true);
+                        cartesian.crosshair()
+                                .yLabel(true);
+
+                        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+
+                        cartesian.title("Blood Pressure");
+                        cartesian.yAxis(0).title("Diastolic/Systolic");
+                        cartesian.xAxis(0).labels().padding(5d, 5d, 5d, 5d);
+
+                        List<DataEntry> seriesData = new ArrayList<>();
+//                        String response_split = response;
+//                        response_split = response_split.substring(0, response_split.length() - 2);
+//                        response_split = response_split.substring(2);
+//                        String[] response_split_full = response_split.split("],\\[");
+//                        for(int i = 0; i< response_split_full.length; i++) {
+//                            String[] rsf_ss = response_split_full[i].split(",");
+//                            seriesData.add(new CustomDataEntry(rsf_ss[0], Integer.parseInt(rsf_ss[1].replaceAll("[^a-zA-Z0-9]", "")), Integer.parseInt(rsf_ss[2].replaceAll("[^a-zA-Z0-9]", ""))));
+//                        }
+                        seriesData.add(new CustomDataEntry("1", 80, 23));
+                        seriesData.add(new CustomDataEntry("2", 120, 40));
+                        seriesData.add(new CustomDataEntry("5", 111, 62));
+                        seriesData.add(new CustomDataEntry("8", 105, 58));
+                        seriesData.add(new CustomDataEntry("11", 101, 70));
+                        seriesData.add(new CustomDataEntry("14", 116, 39));
+                        seriesData.add(new CustomDataEntry("15", 164, 80));
+                        seriesData.add(new CustomDataEntry("16", 180, 33));
+
+                        Set set = Set.instantiate();
+                        set.data(seriesData);
+                        Mapping series1Mapping = set.mapAs("{ x: 'x', value: 'value' }");
+                        Mapping series2Mapping = set.mapAs("{ x: 'x', value: 'value2' }");
+
+                        Line series1 = cartesian.line(series1Mapping);
+                        series1.name("Diastolic");
+                        series1.hovered().markers().enabled(true);
+                        series1.hovered().markers()
+                                .type(MarkerType.CIRCLE)
+                                .size(4d);
+                        series1.tooltip()
+                                .position("right")
+                                .anchor(Anchor.LEFT_CENTER)
+                                .offsetX(5d)
+                                .offsetY(5d);
+
+                        Line series2 = cartesian.line(series2Mapping);
+                        series2.name("Systolic");
+                        series2.hovered().markers().enabled(true);
+                        series2.hovered().markers()
+                                .type(MarkerType.CIRCLE)
+                                .size(4d);
+                        series2.tooltip()
+                                .position("right")
+                                .anchor(Anchor.LEFT_CENTER)
+                                .offsetX(5d)
+                                .offsetY(5d);
+
+                        cartesian.legend().enabled(true);
+                        cartesian.legend().fontSize(13d);
+                        cartesian.legend().padding(0d, 0d, 10d, 0d);
+
+                        anyChartView.setChart(cartesian);
                     }
                 },
                 new Response.ErrorListener() {
@@ -80,47 +139,17 @@ public class GraphFragment extends Fragment {
                 });
 
         queue.add(getRequest);
+
+
         return graph_view;
     }
 
-    private void parseAllData(String response, List<DataEntry> bp_list) {
-       Log.d("Res", response);
-        Set set = Set.instantiate();
-        set.data(seriesData);
-        Mapping series1Mapping = set.mapAs("{ x: 'x', value: 'value' }");
-        
-
-        // Draw Series Line
-        Line series1 = cartesian.line(series1Mapping);
-        series1.name("Brandy");
-        series1.hovered().markers().enabled(true);
-        series1.hovered().markers()
-                .type(MarkerType.CIRCLE)
-                .size(4d);
-        series1.tooltip()
-                .position("right")
-                .anchor(Anchor.LEFT_CENTER)
-                .offsetX(5d)
-                .offsetY(5d);
-
-        // Legend initialize
-        cartesian.legend().enabled(true);
-        cartesian.legend().fontSize(13d);
-        cartesian.legend().padding(0d, 0d, 10d, 0d);
-
-        ///////////TEMP-ADDS////////////////
-        seriesData.add(new CustomDataEntry("1986", 3));
-        seriesData.add(new CustomDataEntry("1987", 7.1));
-        ///////////////////////////////////
-
-
-        anyChartView.setChart(cartesian);
-    }
 
     private class CustomDataEntry extends ValueDataEntry {
 
-        CustomDataEntry(String x, Number value) {
+        CustomDataEntry(String x, Number value, Number value2) {
             super(x, value);
+            setValue("value2", value2);
         }
 
     }
