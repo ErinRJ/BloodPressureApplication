1ba8836096145254b656b3cf9cb3c6f0b0d5512c
AlexMotyka <motykaalex@gmail.com>
2019-11-28 12:51:51 -0500

    Add new medication activity

--- app/src/main/java/com/example/bloodpressureapplication/NewMedicationActivity.java
+++ app/src/main/java/com/example/bloodpressureapplication/NewMedicationActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -17,6 +18,9 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.bloodpressureapplication.Fragments.MedicationFragment;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,6 +29,9 @@ public class NewMedicationActivity extends AppCompatActivity {
     public EditText medNameField;
     public EditText timeField;
 
+    public Button saveBtn;
+    public Button cancelBtn;
+
     public boolean sun = false;
     public boolean mon = false;
     public boolean tues = false;
@@ -32,6 +39,8 @@ public class NewMedicationActivity extends AppCompatActivity {
     public boolean thurs = false;
     public boolean fri = false;
     public boolean sat = false;
+
+    String time;
 
     RequestQueue queue;
 
@@ -42,28 +51,51 @@ public class NewMedicationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_medication);
         medNameField = (EditText)findViewById(R.id.nameVal);
         timeField = (EditText)findViewById(R.id.timeVal);
+        saveBtn = (Button) findViewById(R.id.Save);
+        cancelBtn = (Button) findViewById(R.id.cancel);
         queue = Volley.newRequestQueue(this);
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String medName = medNameField.getText().toString();
+                String timeValue = timeField.getText().toString();
+                Log.d("Time Raw", timeValue);
+
+
+                SimpleDateFormat dateFormat = new SimpleDateFormat("hmmaa");
+                SimpleDateFormat dateFormat2 = new SimpleDateFormat("hh:mm aa");
+
+                try {
+                    Date date = dateFormat.parse(timeValue);
+
+                    time = dateFormat2.format(date);
+                    Log.d("Time", time);
+                } catch (ParseException e) {
+                    Log.e("Time Error", String.valueOf(e));
+                }
+
+                // request the server to create record and then return to main activity
+                createMedicationRecord(medName, time);
+            }
+        });
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NewMedicationActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void cancel(View view) {
-        Intent intent = new Intent(this, MedicationFragment.class);
-        startActivity(intent);
-    }
-
-    public void save(View view) {
-        String medName = medNameField.getText().toString();
-        String timeValue = timeField.getText().toString();
-
-        // request the server to create record and then return to main activity
-        createMedicationRecord(medName, timeValue);
-    }
 
     // TODO: this method will request the server to create a medication record in the db
     public void createMedicationRecord(final String name, final String time) {
 
         // make the request to the server
         // TODO: Change this to our server url
-        String url = "http://httpbin.org/post";
+        String url = server.url + "createMed";
         StringRequest postRequest = new StringRequest(Request.Method.POST, url,
                 new Response.Listener<String>()
                 {
@@ -86,14 +118,28 @@ public class NewMedicationActivity extends AppCompatActivity {
             protected Map<String, String> getParams()
             {
                 Map<String, String> params = new HashMap<String, String>();
-                params.put("name", name);
+
+                int sunday = sun ? 1 : 0;
+                int monday = mon ? 1 : 0;
+                int tuesday = tues ? 1 : 0;
+                int wednesday = wed ? 1 : 0;
+                int thursday = thurs ? 1 : 0;
+                int friday = fri ? 1 : 0;
+                int saturday = sat ? 1 : 0;
+
+                params.put("medName", name);
+                params.put("clientId", "1");
+                params.put("medId", "1");
+
                 params.put("time", time);
-                params.put("mon", String.valueOf(mon));
-                params.put("tues", String.valueOf(tues));
-                params.put("wed", String.valueOf(wed));
-                params.put("thurs", String.valueOf(thurs));
-                params.put("fri", String.valueOf(fri));
-                params.put("sat", String.valueOf(sat));
+
+                params.put("sun", String.valueOf(sunday));
+                params.put("mon", String.valueOf(monday));
+                params.put("tues", String.valueOf(tuesday));
+                params.put("wed", String.valueOf(wednesday));
+                params.put("thurs", String.valueOf(thursday));
+                params.put("fri", String.valueOf(friday));
+                params.put("sat", String.valueOf(saturday));
 
                 return params;
             }
@@ -101,7 +147,7 @@ public class NewMedicationActivity extends AppCompatActivity {
         queue.add(postRequest);
 
         // after the request is sent return to main activity
-        Intent intent = new Intent(this, MedicationFragment.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
 
     }
